9a9c98e035d6dc96028533dc19ac294695393276
Akshat-kumar-jain <iakshatkumarjain@gmail.com>
2022-12-12 23:09:54 +0530

    Created body of Snake 🐍

--- snake.js
+++ snake.js
@@ -1,9 +1,33 @@
 function init(){
+    var canvas = document.getElementById('mycanvas');
+    W = H = canvas.width = canvas.height = 1000;
+    pen = canvas.getContext('2d');
+    cellSize = cs = 67;
+    snake = {
+        init_len:5,
+        color:"blue",
+        cells:[],
+        direction:"right",
 
+        createSnake:function(){
+            for (var i= this.init_len;i>0;i--){
+                this.cells.push({x:i,y:0});
+            }
+        },
+
+        drawSnake:function(){
+
+            for(var i = 0;i<this.cells.length;i++){
+            pen.fillRect(this.cells[i].x*50,this.cells[i].y*50,cs,cs)
+            }
+        }
+    };
+
+    snake.createSnake();
 }
 
 function draw(){
-
+    snake.drawSnake();
 }
 
 function update(){
@@ -11,7 +35,8 @@ function update(){
 }
 
 function gameloop(){
-
+    draw();
+    update();
 }
 
 init();
