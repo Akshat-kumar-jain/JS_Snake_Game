0addcb4cef0590991c82e2bb31f4e49671706f5f
Akshat-kumar-jain <iakshatkumarjain@gmail.com>
2022-12-13 21:53:53 +0530

    Adding moment to the snake!🐒

--- snake.js
+++ snake.js
@@ -19,20 +19,76 @@ function init(){
         drawSnake:function(){
 
             for(var i = 0;i<this.cells.length;i++){
-            pen.fillRect(this.cells[i].x*50,this.cells[i].y*50,cs,cs)
+                pen.fillStyle = this.color;
+            pen.fillRect(this.cells[i].x*50,this.cells[i].y*50,cs - 20,cs)
             }
+        },
+
+        updateSnake:function(){
+            console.log("updateSnake according to the direction property");
+
+
+            this.cells.pop();
+
+            var headX = this.cells[0].x;
+            var headY = this.cells[0].y;
+            var nextX,nextY;
+
+            if(this.direction=="right"){
+                nextX = headX + 1;
+                nextY = headY;
+            }
+            else if(this.direction=="left"){
+                nextX = headX - 1;
+                nextY = headY;
+            }
+            else  if(this.direction=="down"){
+                nextX = headX ;
+                nextY = headY + 1;
+            }
+            else{
+                nextX = headX ;
+                nextY = headY - 1 ;
+            }
+            /*var X = headX + 1;
+            var Y = headY;*/
+            this.cells.unshift({x:nextX,y:nextY});
         }
     };
 
     snake.createSnake();
+
+    // Add a event listner on the Document Object
+    function keyPressed(e){
+        console.log("KeyPressed",e.key);
+
+        //Conditional Statements
+        if(e.key =="ArrowRight"){
+            snake.direction= "right"
+        }
+        if(e.key =="ArrowLeft"){
+            snake.direction= "left"
+        }
+        if(e.key =="ArrowUp"){
+            snake.direction= "up"
+        }
+        if(e.key =="ArrowDown"){
+            snake.direction= "down"
+        }
+    }
+    document.addEventListener('keydown',keyPressed);
+
 }
 
 function draw(){
+
+    // erase the old frame
+    pen.clearRect(0,0,W,H);
     snake.drawSnake();
 }
 
 function update(){
-
+    snake.updateSnake();
 }
 
 function gameloop(){
